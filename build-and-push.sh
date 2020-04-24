d092d465a2aaaae7d92c6d4d20a9a2e1ab65ff2b
Dexter-Yoon <49930827+Dexter-Yoon@users.noreply.github.com>
2020-04-24 15:29:27 +0900

    Update build-and-push.sh

--- build-and-push.sh
+++ build-and-push.sh
@@ -2,6 +2,6 @@
 DOCKERHUB_ACCOUNT="admin"
 DOCKERURL="kubelb01:5000"
 DOCKER_IMAGE="nginx-example"
-docker login -u ${DOCKERHUB_ACCOUNT}
+docker login -u ${DOCKERHUB_ACCOUNT} https://${DOCKERURL}/v1/
 docker build -t ${DOCKERURL}/${DOCKER_IMAGE}:latest -f applications/test-webapp-1/Dockerfile applications/test-webapp-1/
 docker push ${DOCKERURL}/${DOCKER_IMAGE}:latest
